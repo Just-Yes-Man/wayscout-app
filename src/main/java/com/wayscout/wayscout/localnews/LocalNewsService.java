56a1b236db0988f52b140138c4bce4a40d1ad2af
SoyManu3 <manu262005@gmail.com>
2026-05-13 17:51:39 -0600

    add cambios apartado de noticias

--- src/main/java/com/wayscout/wayscout/localnews/LocalNewsService.java
+++ src/main/java/com/wayscout/wayscout/localnews/LocalNewsService.java
@@ -49,7 +49,10 @@ public class LocalNewsService {
     public LocalNewsService() {
         this.restClient = RestClient.builder()
                 .baseUrl("https://news.google.com")
-                .defaultHeader("User-Agent", "WayScout/1.0")
+                .defaultHeader("User-Agent", "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36")
+                .defaultHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+                .defaultHeader("Accept-Language", "es-MX,es;q=0.9,en;q=0.8")
+                .defaultHeader("Accept-Encoding", "identity")
                 .build();
     }
 
